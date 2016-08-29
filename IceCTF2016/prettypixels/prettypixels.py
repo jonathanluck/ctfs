2397297f724144a55c8b4310ccd2c70a1863a852
Jonathan Luck <jrluck@hotmail.com>
2016-08-28 20:43:03 -0700

    Added Pretty Pixel Files

---
+++ IceCTF2016/prettypixels/prettypixels.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import binascii
+
+def c(n):
+    if(n < 16):
+        return ("0" + hex(n)[2:])
+    return (hex(n)[2:])
+
+def hexpix(p):
+    out = ""
+    out += c(p[0])
+    out += c(p[1])
+    out += c(p[2])
+    return out
+
+
+byte = ""
+
+
+with Image.open("pretty_pixels.png") as img:
+    width, height = img.size
+    for y in range(0,height):
+        for x in range(0,width):
+            d = img.getpixel((x,y))
+            byte+= hexpix(d)
+
+open("pretty_pixels_out.png",'wb').write(binascii.unhexlify(byte))
+
